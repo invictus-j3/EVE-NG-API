7550c5b64ce27ae47e9df70a5960d9a484c6aede
invictus <github@jasonguynn.net>
2023-06-07 21:31:57 -0500

    add Note_start.py

--- node_start.py
+++ node_start.py
@@ -7,9 +7,7 @@ def main():
     with open('environment.json') as e:
         environ = json.load(e)
         e.close()
-
-    time.sleep(300)
-    
+   
     eve=eve_ng()
     eve.start_lab_nodes(environ['lab']['name'])
 
