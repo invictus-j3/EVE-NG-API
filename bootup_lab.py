84a3b97d60a1b5cef48439e15fe9e6269152517a
invictus <github@jasonguynn.net>
2023-06-07 21:20:45 -0500

    Create bootup_lab.py

---
+++ bootup_lab.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import json, time
+from eve_ng import eve_ng
+
+def main():
+    with open('environment.json') as e:
+        environ = json.load(e)
+        e.close()
+
+    time.sleep(300)
+    
+    eve=eve_ng()
+    eve.start_lab_nodes(environ['lab']['name'])
+
+if __name__ == '__main__':
+    main()
